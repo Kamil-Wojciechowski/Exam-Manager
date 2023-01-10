1a177e6050f5fafcd282442c20f574c514d5b5e7
Kamil-Wojciechowski <wojciechowski.kamil1@outlook.com>
2023-01-10 22:04:03 +0100

    Initialization

---
+++ src/main/java/com/wojcka/exammanager/ExamManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wojcka.exammanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wojcka/exammanager/ExamManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wojcka.exammanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
